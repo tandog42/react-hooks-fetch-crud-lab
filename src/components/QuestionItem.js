d012209d9d520052e5023d264b05529afc0bb9c2
Tanner <troxs690@gmail.com>
2022-07-08 11:53:34 -0500

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteIds, patchAnswers }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,28 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(e) {
+    
+
+    deleteIds(id);
+  }
+  function handleChange(e) {
+  
+    patchAnswers(id, e.target.value);
+   console.log(id)
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,60 @@
 import React from "react";
 
-function QuestionList() {
+import QuestionItem from "./QuestionItem";
+
+function QuestionList({ questions, setQuestions }) {
+  function questionDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(r => r.json())
+      .then(() => {
+        const deleteId = questions.filter(q => q.id !== id);
+
+        setQuestions(deleteId);
+      });
+  }
+
+  function patchAnswers(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: correctIndex,
+      }),
+    })
+      .then(r => r.json())
+      .then(num => {
+        const allQuestions = questions.map(questionAll => {
+          if (questionAll.id === num.id) {
+            return num;
+          } else {
+            return questionAll;
+          }
+        });
+        // console.log(allQuestions)
+        setQuestions(allQuestions);
+      });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+
+      <ul>
+        {questions.map(qq => (
+          // console.log(qq.id)
+          <QuestionItem
+            patchAnswers={patchAnswers}
+            deleteIds={questionDelete}
+            key={qq.id}
+            id={qq.id}
+            question={qq}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
